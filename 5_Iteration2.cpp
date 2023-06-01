35118cd6611eae4da08b5deef1b4ceec886b5c46
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-06-01 15:26:06 +0530

    Create 5_Iteration2.cpp

---
+++ 5_Iteration2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 0;
+
+  // In this program we use while loop
+  
+   while(iCnt<=4)
+   {
+        cout<<"\n"<<iCnt ;
+        iCnt++;
+   } 
+}
