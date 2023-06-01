b21ff6c09492c2c6e908d8ce0219842a767d1d67
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-06-01 15:36:02 +0530

    Create 6_Iteration3.cpp

---
+++ 6_Iteration3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 0 ;
+    iCnt = 1;
+
+  // In this program we use do while loop
+    do
+    {
+        cout<<"\n"<<iCnt;
+        iCnt++;
+
+    }
+    while(iCnt<=5);
+
+
+}
